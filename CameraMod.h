4722b37c7663b1641a7c9747eddfd4f646b61e08
Zach <zach.richard94@gmail.com>
2014-05-02 15:09:31 -0400

    Just the file I'll be using. Nothing but a description for now.
    ~Zach

---
+++ CameraMod.h
@@ -0,0 +1,8 @@
+/* #Trap Sat CameraModule
+ *   Author: Zach Ricahard
+ *   Capitol College 2014
+ *
+ *   This file will include the functions necessary to run the camera on
+ *  the Arduino Yun.
+ */
+
